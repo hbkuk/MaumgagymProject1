da83da0208de1b3cb2db21170057c2c630110367
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-02-06 08:35:15 +0900

    [update] 파라미터 수정 필요

--- Maumgagym/src/main/webapp/facility/facility_source/facilityList.js
+++ Maumgagym/src/main/webapp/facility/facility_source/facilityList.js
@@ -9,25 +9,18 @@ $(document).ready(function() {
     	
     	const urlParams = new URLSearchParams();
     	//console.log( urlParams );
-    	urlParams.set('category_seq', categorySeq );
+    	//urlParams.set('category_seq', categorySeq );
     	
     	
-    	// 현재 url 가져오기
-    	//let url = $(location).attr('href');
-    	//console.log( url );
-    	
-    	// 현재 url에 파라미터 붙이기
-    	//url += "&category_seq=" + categorySeq
-    	
-    	// 파라미터 설정 후 이동시키기
-    	 //location.href = url;
-    	
-    	if( urlParams.has( "dongAddr" ) ) {
-    	
-    		location.href = url + "&" + urlParams.toString();
+    	if( urlParams.has( "category_seq" ) ) {
+    		
+    		urlParams.delete('category_seq');
+    		urlParams.append('category_seq', categorySeq );
+    		
+    		location.href = url + urlParams.toString();
     	
     	} else {
-			urlParams.delete("category_seq")
+			urlParams.set('category_seq', categorySeq );
 			location.href = url + "?" + urlParams.toString();
 			
 		}

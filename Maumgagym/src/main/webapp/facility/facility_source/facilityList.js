b7da76310b03e3e97ddb2374a4eea45e3c0fa37a
B.Kuk <109803585+hbkuk@users.noreply.github.com>
2023-02-06 09:59:47 +0900

    [update] facility_list 조건에 따른 파라미터 변경

--- Maumgagym/src/main/webapp/facility/facility_source/facilityList.js
+++ Maumgagym/src/main/webapp/facility/facility_source/facilityList.js
@@ -2,29 +2,62 @@ $(document).ready(function() {
 	
 	$("li").click(function() {
 		
+		// 카테고리 클릭 -> get 해당 카테고리 번호
     	let categorySeq = $(this).children("a").attr("id");
-    	//console.log( categorySeq );
     	
-    	const url = window.location.href;
+    	// 현재 url
+    	const urlStr = window.location.href;
     	
-    	const urlParams = new URLSearchParams();
-    	//console.log( urlParams );
-    	//urlParams.set('category_seq', categorySeq );
+    	const url = new URL(urlStr);
     	
-    	
-    	if( urlParams.has( "category_seq" ) ) {
+    	// 파라미터 객체 생성
+    	const urlParams = url.searchParams;
+		
+		// 객체 로그 확인
+		//console.log( urlParams );
+		
+		// dongAddr 파라미터 확인
+		//console.log (urlParams.has("dongAddr") );
+		
+		// dongAddr O , category_seq X 		=>		 카테고리 번호 파라미터 추가
+    	if( urlParams.has( "dongAddr" ) && !( urlParams.has( "category_seq" ) ) ) {
+	
+	
+		   // 파라미터 객체 생성
+    	   const urlParams = url.searchParams;
     		
-    		urlParams.delete('category_seq');
-    		urlParams.append('category_seq', categorySeq );
-    		
-    		location.href = url + urlParams.toString();
-    	
-    	} else {
-			urlParams.set('category_seq', categorySeq );
-			location.href = url + "?" + urlParams.toString();
+    		urlParams.delete('dongAddr');
+    		urlParams.set('category_seq', categorySeq );
 			
-		}
+    		location.href = urlStr + "&" + urlParams.toString();
     	
+    	// dongAddr X , category_seq O		=>		카테고리 번호 파라미터 변경
+    	} else if ( !(urlParams.has( "dongAddr" )) && ( urlParams.has( "category_seq" ) ) ) {
+	
+			
+			// search 프로퍼티를 빈 문자열('')로 설정
+			url.search = '';
+			
+		    // 파라미터 객체 생성
+    	   const urlParams = url.searchParams;
+    	   
+			urlParams.append('category_seq', categorySeq );
+			location.href = url;
+		
+		// dongAddr X , category_seq X		=>		카테고리 번호 파라미터 추가
+		}  else if ( !(urlParams.has( "dongAddr" )) && !(urlParams.has( "category_seq" ) ) ) {
+		
+			// 파라미터 객체 생성
+    	    const urlParams = url.searchParams;
+    	   
+			urlParams.append('category_seq', categorySeq );
+			location.href = url;
+		
+		
+		// dongAddr X , category_seq X		=>		카테고리 번호 파라미터 변경	
+		}  else if ( !(urlParams.has( "dongAddr" )) && ( urlParams.has( "category_seq" ) ) ) {
+    	
+    	}
     });
     
 });
